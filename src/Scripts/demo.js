f866f7e50a951c5dfd39b81e4ccac317639f03ef
Federico A. Urraca <65458679+Federico1196@users.noreply.github.com>
2023-05-12 22:38:55 -0400

    update

--- src/Scripts/demo.js
+++ src/Scripts/demo.js
@@ -97,23 +97,42 @@ var Cars = {
         918:["2015"],
         "Carrera GT":["2006"]
     }
-
-
-
-
-
 }
 
 
 window.onload = function(){
     const selectMake = document.getElementById('make'),
-        selectModel = document.getElementById('model'),
-        selectYear = document.getElementById('year'),
-        selects = document.querySelectorAll('select')
+          selectModel = document.getElementById('model'),
+          selectYear = document.getElementById('year'),
+          selects = document.querySelectorAll('select');
+    
+    const searchForm = document.getElementById('search-form');
+    const searchButton = document.getElementById('search-button');
 
-        selectModel.disabled = true
+    selectModel.disabled = true //Makes sure the buttons are disable until selection its made
+    selectYear.disabled = true
+
+
+    selects.forEach(select => {
+        if(select.disabled == true){
+            select.style.cursor = "auto"
+        }
+        else{
+            select.style.cursor = "pointer"
+        }
+    })
+
+    for(let make in Cars){
+        // console.log(make);
+        selectMake.options[selectMake.options.length] = new Option(make, make)
+    }
+
+
+    // make change
+    selectMake.onchange = (e) =>{
+        selectModel.disabled = false
         selectYear.disabled = true
-  
+        // selectZip.disabled = true
 
         selects.forEach(select => {
             if(select.disabled == true){
@@ -124,56 +143,35 @@ window.onload = function(){
             }
         })
 
-        for(let make in Cars){
-            // console.log(make);
-            selectMake.options[selectMake.options.length] = new Option(make, make)
+        selectModel.length = 1
+        selectYear.length = 1
+        // selectZip.length = 1
+
+        for(let model in Cars[e.target.value]){
+            // console.log(model);
+            selectModel.options[selectModel.options.length] = new Option(model, model)
         }
+    }
 
 
-        // make change
-        selectMake.onchange = (e) =>{
-            selectModel.disabled = false
-            selectYear.disabled = true
-            // selectZip.disabled = true
-
-            selects.forEach(select => {
-                if(select.disabled == true){
-                    select.style.cursor = "auto"
-                }
-                else{
-                    select.style.cursor = "pointer"
-                }
-            })
-
-            selectModel.length = 1
-            selectYear.length = 1
-            // selectZip.length = 1
-
-            for(let model in Cars[e.target.value]){
-                // console.log(model);
-                selectModel.options[selectModel.options.length] = new Option(model, model)
+    selectModel.onchange = (e) => {
+        selectYear.disabled = false;
+        
+        selects.forEach((select) => {
+            if (select.disabled == true) {
+            select.style.cursor = "auto";
+            } else {
+            select.style.cursor = "pointer";
             }
+        });
+        
+        selectYear.length = 1;
+        
+        let Year = Cars[selectMake.value][e.target.value].filter(y => parseInt(y) >= 2016);
+        
+        for (let i = 0; i < Year.length; i++) {
+            selectYear.options[selectYear.options.length] = new Option(Year[i], Year[i]);
         }
+    };
 
-
-        selectModel.onchange = (e) => {
-            selectYear.disabled = false;
-          
-            selects.forEach((select) => {
-              if (select.disabled == true) {
-                select.style.cursor = "auto";
-              } else {
-                select.style.cursor = "pointer";
-              }
-            });
-          
-            selectYear.length = 1;
-          
-            let Year = Cars[selectMake.value][e.target.value].filter(y => parseInt(y) >= 2016);
-          
-            for (let i = 0; i < Year.length; i++) {
-              selectYear.options[selectYear.options.length] = new Option(Year[i], Year[i]);
-            }
-        };
-
-}
+};
